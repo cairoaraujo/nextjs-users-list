21a258219dfce9e894beda38eaea8bbea9159f8e
Cairo Araujo <cairobezerra@hotmail.com>
2025-08-11 00:39:37 -0300

    update env

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,4 @@
-export const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:3001';
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 export const ENDPOINTS = {
   USERS: `${API_BASE_URL}/users`,
